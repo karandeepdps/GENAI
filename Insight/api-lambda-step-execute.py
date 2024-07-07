60058d6c21793e7b25b38956e966cc5bf9c03858
KD <karandeepdps@gmail.com>
2024-07-07 20:53:24 -0230

    added emai

--- Insight/api-lambda-step-execute.py
+++ Insight/api-lambda-step-execute.py
@@ -6,10 +6,11 @@ step_functions_client = boto3.client('stepfunctions')
 def lambda_handler(event, context):
     body = json.loads(event['body'])
     youtube_url = body['youtube_url']
+    recipient_email = body['recipient_email']
     
     response = step_functions_client.start_execution(
         stateMachineArn='arn:aws:states:us-east-1:590184144410:stateMachine:MyStateMachine-dm1ayly4p', # Update to your Step Function ARN
-        input=json.dumps({'youtube_url': youtube_url})
+        input=json.dumps({'youtube_url': youtube_url, 'recipient_email': recipient_email})
     )
     
     return {
@@ -23,4 +24,3 @@ def lambda_handler(event, context):
             'executionArn': response['executionArn']
         })
     }
-
